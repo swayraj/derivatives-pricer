9e05d7088e97a4013b698c85d40bcaa60cf47f73
swayraj <swarajdhayarkar@gmail.com>
2025-10-13 16:14:53 +0530

    Updated app.py to listen to external requests

--- volatility-service/app.py
+++ volatility-service/app.py
@@ -63,4 +63,4 @@ def predict():
 
 if __name__ == '__main__':
     # Starts the development server on port 5000
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
